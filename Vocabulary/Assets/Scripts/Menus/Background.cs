2f086987d0b2cce64a8dca0c49e8d510f044828a
Hiz <leeh14@rpi.edu>
2015-10-23 00:26:14 -0400

    Attached new gui to parts of combat system

---
+++ Vocabulary/Assets/Scripts/Menus/Background.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Background : MonoBehaviour {
+	private Sprite  temp;
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	void Start()
+	{
+	}
+	public void LoadCombatBG()
+	{
+		temp = Resources.Load<Sprite>("Backgrounds/UI_BattleScreen_dungeon1") ;
+		gameObject.GetComponent<Image>().sprite = temp;
+
+	}
+	public void LoadQuestionbg1()
+	{
+		temp = Resources.Load<Sprite>("Backgrounds/UI_QuestionScreen_Background1");
+		gameObject.GetComponent<Image>().sprite = temp;
+	}
+	public void LoadCrafting()
+	{
+		temp = Resources.Load<Sprite>("Backgrounds/UI_CraftingScreen_background") ;
+		gameObject.GetComponent<Image>().sprite = temp;
+	}
+	public void LoadEquipment()
+	{
+		temp = Resources.Load<Sprite>("Backgrounds/UI_EquipmentScreen_Background") ;
+		gameObject.GetComponent<Image>().sprite = temp;
+	}
+	public void LoadStart()
+	{
+		temp = Resources.Load<Sprite>("Backgrounds/UI_StartScreen_Background") ;
+		gameObject.GetComponent<Image>().sprite = temp;
+	}
+}
